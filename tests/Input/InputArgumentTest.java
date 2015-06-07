a6a4eba9702ffa8ef0c6f0e33c2b80fd02f98275
bound1ess <bound1ess.dev@gmail.com>
2015-06-08 02:21:34 +0600

    Write more tests for InputArgument.

--- tests/Input/InputArgumentTest.java
+++ tests/Input/InputArgumentTest.java
@@ -9,4 +9,14 @@ public class InputArgumentTest {
     public void returnsArgumentName() {
         assertEquals(new InputArgument("foo", true).getName(), "foo");
     }
+
+    @Test
+    public void returnsTrueIfRequired() {
+        assertEquals(new InputArgument("foo", true).isRequired(), true);
+    }
+
+    @Test
+    public void returnsFalseIfNotRequired() {
+        assertEquals(new InputArgument("foo", false).isRequired(), false);
+    }
 }
