5c22744bda78463e94519988f8ff6026dd312f1b
bound1ess <bound1ess.dev@gmail.com>
2015-06-11 00:49:36 +0600

    Work on Main.

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,6 @@ import trail.filesystem.Reader;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.HashMap;
 
 import java.nio.file.FileSystems;
@@ -40,10 +39,7 @@ public class Main {
         // If debug is enabled, dump config map.
         if (config.get("debug").equals("yes")) {
             System.out.println("Dumping current configuration...");
-
-            for (Entry<String, String> entry: config.entrySet()) {
-                System.out.println(entry.getKey() + ": " + entry.getValue());
-            }
+            System.out.println(config.toString());
         }
 
         String workDir = config.get("dir");
@@ -52,25 +48,40 @@ public class Main {
         System.out.println("Trail (ver. " + VERSION + ") is running...");
         System.out.println("Current working directory is " + workDir);
 
-        Finder finder = new Finder(loadExtensions(workDir));
+        List<String> extensions = loadExtensions(workDir);
+        Finder finder = new Finder(extensions);
+
+        // If debug is enabled, print all extensions.
+        if (config.get("debug").equals("yes")) {
+            System.out.println("Extensions: " + extensions.toString());
+        }
 
         for (String filePath: finder.recursiveTraversal(Paths.get(workDir))) {
             if (config.get("verbose").equals("yes")) {
-                System.out.println(filePath);
-            }
+                System.out.print("Checking " + filePath + "...");
+
+                if (config.get("fix").equals("yes")) {
+                    // Fix the file
+                    // @todo check if fixed
+                    System.out.println(" Fixed.");
+                } else {
+                    System.out.println("");
+                }
+            } // check if valid
         }
 
         // Calculate the time.
         long executionTime = System.currentTimeMillis() - startTime;
-        System.out.printf("Finished in %.2f seconds.\n", executionTime / 1000.0);
+        System.out.printf("Finished in %.2f second(s).\n", executionTime / 1000.0);
     }
 
     private static List<String> loadExtensions(final String srcDir) {
         Path filePath = FileSystems.getDefault().getPath(srcDir);
 
         List<String> extensions = new ArrayList<>();
+        List<String> lines = Reader.listOfLines(filePath);
 
-        if ( ! Files.isReadable(filePath)) {
+        if (lines == null) {
             for (String extension: DEFAULT_EXTENSIONS) {
                 extensions.add(extension);
             }
@@ -78,7 +89,6 @@ public class Main {
             return extensions;
         }
 
-        List<String> lines = Reader.listOfLines(filePath);
         boolean keepDefault = lines.size() == 0 || lines.get(0).equals("keep");
 
         if (lines.size() > 0) {
